a85c444b4e4daedbeac84b8c342e97139c1e8e1b
HGKczky <HGKczky@gmail.com>
2024-01-06 21:57:23 -0500

    Added Test Hello World

--- MediaDownloader.java
+++ MediaDownloader.java
@@ -3,18 +3,19 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.FileOutputStream;
-import java.io.FilenameFilter;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.URL;
 
 public class MediaDownloader{
+    static JLabel WarningTag;
+    
     public static void main(String[] args) {
         MakeMainWindow();
     }
-
     public static void MakeMainWindow(){
-
+	
+	System.out.println("Hello World");
         ImageIcon backgroundImage = new ImageIcon("background.png");
         JLabel backgroundLabel = new JLabel(backgroundImage);
         JFrame frame = new JFrame("Media Downloader");
@@ -23,44 +24,58 @@ public class MediaDownloader{
         frame.setSize(backgroundImage.getIconWidth(), backgroundImage.getIconHeight());
         frame.setResizable(false);
 
-        JTextField TextField = new JTextField();
-        TextField.setBounds(((backgroundImage.getIconWidth() - 300) / 2), 400, 300, 50);
-        TextField.setFont(new Font("Arial", Font.PLAIN, 20));
+        JLabel ProgramName = new JLabel("Media Downloader");
+        ProgramName.setBounds(((backgroundImage.getIconWidth() - 600) / 2), 100, 600, 100);
+        ProgramName.setFont(new Font("Arial", Font.BOLD, 60));
+        ProgramName.setHorizontalAlignment(SwingConstants.CENTER); 
 
-        JTextField MediaName = new JTextField();
-        MediaName.setBounds(((backgroundImage.getIconWidth() - 300) / 2), 300, 300, 50);
-        MediaName.setFont(new Font("Arial", Font.PLAIN, 20));
+        JTextField LinkField = new JTextField();
+        LinkField.setBounds(((backgroundImage.getIconWidth() - 300) / 2), 300, 300, 50);
+        LinkField.setFont(new Font("Arial", Font.PLAIN, 20));
 
+        JTextField NameField = new JTextField();
+        NameField.setBounds(((backgroundImage.getIconWidth() - 300) / 2), 400, 300, 50);
+        NameField.setFont(new Font("Arial", Font.PLAIN, 20));
 
-        JButton DownloadButton = new JButton("Dowload Media");
+        JLabel nameLabel = new JLabel("Enter Link");
+        nameLabel.setBounds(((backgroundImage.getIconWidth() - 250) / 2), 260, 250, 30);
+        nameLabel.setFont(new Font("Arial", Font.BOLD, 25));
+        nameLabel.setHorizontalAlignment(SwingConstants.CENTER); 
+
+        JLabel linkLabel = new JLabel("Enter Filename");
+        linkLabel.setBounds(((backgroundImage.getIconWidth() - 250) / 2), 360, 250, 30);
+        linkLabel.setFont(new Font("Arial", Font.BOLD, 25));
+        linkLabel.setHorizontalAlignment(SwingConstants.CENTER); 
+
+        WarningTag = new JLabel(" ");
+        WarningTag.setBounds(((backgroundImage.getIconWidth() - 600) / 2), 560, 600, 30);
+        WarningTag.setFont(new Font("Arial", Font.BOLD, 15));
+        WarningTag.setForeground(Color.RED);
+        WarningTag.setHorizontalAlignment(SwingConstants.CENTER); 
+
+        JButton DownloadButton = new JButton("Dowload Media"); 
         DownloadButton.setBounds(((backgroundImage.getIconWidth() - 300) / 2), 500, 300, 50);
         DownloadButton.setBackground(Color.white);
-        DownloadButton.setFont(new Font("Arial", Font.BOLD, 30));
+        DownloadButton.setFont(new Font("Arial", Font.BOLD, 25));
         backgroundLabel.add(DownloadButton);
         DownloadButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                String linkname = TextField.getText();
-                String Filename = MediaName.getText();
+                String linkname = LinkField.getText();
+                String Filename = NameField.getText();
                 if (Filename == null){
                     System.out.println("Please Enter a filename");
                 }
                 DownloadMedia(linkname, Filename);
             }       
         });
-        JLabel linkLabel = new JLabel("Enter Link:");
-        linkLabel.setBounds(((backgroundImage.getIconWidth() - 300) / 2), 260, 250, 30);
-        linkLabel.setFont(new Font("Arial", Font.BOLD, 30));
-        backgroundLabel.add(linkLabel);
 
-        JLabel nameLabel = new JLabel("Enter Filename:");
-        nameLabel.setBounds(((backgroundImage.getIconWidth() - 300) / 2), 360, 250, 30);
-        nameLabel.setFont(new Font("Arial", Font.BOLD, 30));
-
+        backgroundLabel.add(ProgramName);
+        backgroundLabel.add(WarningTag);
         backgroundLabel.add(linkLabel);
         backgroundLabel.add(nameLabel);
-        backgroundLabel.add(TextField);
-        backgroundLabel.add(MediaName);
+        backgroundLabel.add(LinkField);
+        backgroundLabel.add(NameField);
         backgroundLabel.add(DownloadButton);
         frame.setVisible(true);
     }
@@ -79,8 +94,8 @@ public class MediaDownloader{
             outputstr.close();
             
         }
-        catch (Exception exception){
-            System.out.println(exception);
+        catch (Exception exception ){
+            WarningTag.setText("Error: " + exception.getMessage() + " is not a valid link.");
         };
     }
 }
